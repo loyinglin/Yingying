0cf2c72e3ba290c84cc1ecc602fc445054ace960
林伟池 <loying@foxmail.com>
2016-07-04 00:06:54 +0800

    删除无用的文件

--- Yingying/vc/myCenter/investment/MyInvestmentViewController.m
+++ Yingying/vc/myCenter/investment/MyInvestmentViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view.
     self.myTabbarController= self.childViewControllers[0];
     self.myTabbarController.tabBar.hidden = YES;
+    
 }
 
 - (void)didReceiveMemoryWarning {
